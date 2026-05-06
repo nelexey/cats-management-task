62329b8ee90a968a3c5a7ebc9c5fce10bcc038b2
nelexey <whoami.nelx@gmail.com>
2026-05-06 17:10:42 +0300

    feat(pets): реализация views, настройка маршрутизации для CRUD и регистрация приложения

--- backend/pets/views.py
+++ backend/pets/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from .models import Cat
+from .serializers import CatSerializer
 
-# Create your views here.
+class CatViewSet(viewsets.ModelViewSet):
+    serializer_class = CatSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Cat.objects.filter(owner=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
