e0716b97d69d57b11740ba34527d3f5ba236e168
nelexey <whoami.nelx@gmail.com>
2026-05-06 16:49:23 +0300

    feat(pets): добавление модели Cat со связью с пользователем

--- backend/pets/models.py
+++ backend/pets/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Cat(models.Model):
+    COLOR_CHOICES = (
+        ('black', 'черный'),
+        ('colored', 'цветной'),
+    )
+    GENDER_CHOICES = (
+        ('M', 'кот'),
+        ('F', 'кошка'),
+    )
+
+    name = models.CharField(max_length=100)
+    birth_date = models.DateField()
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
+    color = models.CharField(max_length=10, choices=COLOR_CHOICES)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='cats')
+
+    def __str__(self):
+        return self.name
