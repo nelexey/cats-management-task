9a046194bb08f670c29a0e69c897a4b90c76f582
nelexey <whoami.nelx@gmail.com>
2026-05-06 17:50:00 +0300

    test(users): реализация API тестов для регистрации и выдачи JWT

--- backend/users/tests.py
+++ backend/users/tests.py
@@ -1,3 +1,28 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+User = get_user_model()
+
+class UserAuthTests(APITestCase):
+    def setUp(self):
+        self.register_url = reverse('register')
+        self.login_url = reverse('token_obtain_pair')
+        self.user_data = {
+            'username': 'testuser',
+            'password': 'StrongPassword123!'
+        }
+
+    def test_user_registration(self):
+        response = self.client.post(self.register_url, self.user_data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(User.objects.count(), 1)
+        self.assertEqual(User.objects.get().username, 'testuser')
+
+    def test_user_login(self):
+        User.objects.create_user(**self.user_data)
+        response = self.client.post(self.login_url, self.user_data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIn('access', response.data)
+        self.assertIn('refresh', response.data)
